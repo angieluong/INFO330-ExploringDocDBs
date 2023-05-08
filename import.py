34122e48a02656ed49c0058e17b7786779f06d0b
angieluong <angielena0212@gmail.com>
2023-05-07 22:31:01 -0700

    completed writing code in order to fulfill basic requirements

---
+++ import.py
@@ -0,0 +1,48 @@
+import sqlite3  # This is the package for all sqlite3 access in Python
+from pymongo import MongoClient
+
+# Connect to MongoDB - Provided in lab
+mongoClient = MongoClient("mongodb://localhost/pokemon")
+pokemonDB = mongoClient['pokemondb']
+pokemonColl = pokemonDB['pokemon_data']
+
+conn = sqlite3.connect('./pokemon.sqlite')
+cursor = conn.cursor()
+
+general_query = """
+SELECT name, pokedex_number, hp, attack, defense, speed,
+sp_attack, sp_defense
+FROM pokemon
+"""
+cursor.execute(general_query)
+
+for row in cursor.fetchall():
+    name, pokedex_number, hp, attack, defense, speed, sp_attack, sp_defense = row
+    abilities_query = """
+    SELECT ability.name, pokemon.pokedex_number
+    FROM ability
+    JOIN pokemon_abilities ON pokemon_abilities.ability_id = ability.id
+    JOIN pokemon ON pokemon_abilities.pokemon_id = pokemon.id
+    WHERE pokedex_number = ?
+    """
+    abilities = [name[0] for name in conn.execute(abilities_query, (pokedex_number,))]
+    cursor.execute("SELECT * FROM pokemon_types_view WHERE name = ?", (name,))
+    pokemon_type_info = cursor.fetchone()
+
+    type_1 = pokemon_type_info[1]
+    type_2 = pokemon_type_info[2]
+
+    pokemon = {
+        "name": name,
+        "pokedex_number": pokedex_number,
+        "types": [type_1, type_2],
+        "hp": hp,
+        "attack": attack,
+        "defense": defense,
+        "speed": speed,
+        "sp_attack": sp_attack,
+        "sp_defense": sp_defense,
+        "abilities": abilities,
+    }
+
+    pokemonColl.insert_one(pokemon)
---
+++ queries.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+
+mongoClient = MongoClient("mongodb://localhost/pokemon")
+pokemonDB = mongoClient['pokemondb']
+pokemonColl = pokemonDB['pokemon_data']
+
+# Write a query that returns all the Pokemon named "Pikachu"
+def pikachu():
+    pikachu = pokemonColl.find_one({"name": "Pikachu"}, projection={"_id": False})
+    print(pikachu)
+
+# Write a query that returns all the Pokemon with an attack greater than 150
+def greater_than_150():
+    greater_than = pokemonColl.find({"attack": {"$gt": 150}})
+    for pokemon in greater_than:
+        print(pokemon)
+
+
+# Write a query that returns all the Pokemon with an ability of "Overgrow"
+def overgrow():
+    overgrow_ability = pokemonColl.find({"abilities": {"$in": ["Overgrow"]}})
+    for pokemon in overgrow_ability:
+        print(pokemon)
+
+# pikachu()
+# greater_than_150()
+overgrow()
+
+
+# mongo_client.close()
